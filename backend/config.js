a833828e13b51f66d5dbddae2126bae870a8ca7e
kwon-23 <bhkwon@ucsd.edu>
2024-10-08 12:43:18 -0700

    MongoDB/mongoose
    Added functionality as well as a Book schema for mongoose/MongoDB to work

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+  'mongodb+srv://briankwon382:<PASS>@book-store.hyybe.mongodb.net/books_collection?retryWrites=true&w=majority&appName=book-store'
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,68 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Test for working local website');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+
+// HTTP route to save a new book
+app.post('/books', async(request, response) => {
+  try{
+    if (
+      !request.body.title || 
+      !request.body.author || 
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, and year published',
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch(error) {
+    console.log(error.message);
+    response.status(500).send({message: error.message});
+  }
 });
 
+// Route to get all books from the MongoDB database (GET method)
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error. message });
+  }
+});
+
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
